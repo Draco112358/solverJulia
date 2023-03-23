e8f619079a994614d44608b72dee879a778f2094
edgardovittoria <edgardovittoria@hotmail.it>
2023-03-23 10:46:53 +0100

    completamento del porting delle funzioni nel file solver_function.py nel linguaggio julia (con dubbi)

--- solver/src/solver_function.jl
+++ solver/src/solver_function.jl
@@ -1,5 +1,7 @@
 using SparseArrays
 using LinearAlgebra
+using IterativeSolvers
+using LinearMaps
 
 struct escals
     Lp
@@ -436,9 +438,9 @@ end
 
 function precond_3_3_vector(LU_S,invZ,invP,A,Gamma,w,X1,X2,X3)
 
-    n1=length(X1)
-    n2=length(X2)
-    n3=length(X3)
+    n1=ifelse(length(X1) > 0, length(X1), 1)
+    n2=ifelse(length(X2) > 0, length(X1), 1)
+    n3=ifelse(length(X3) > 0, length(X1), 1)
 
     i1=range(1, stop=n1)
     i2=range(n1,stop=n1+n2)
@@ -504,4 +506,183 @@ function precond_3_3_Kt(LU_S, invZ, invP, A,Gamma, n1,n2, X3)
     Y[np.ix_(i3)] = Y[np.ix_(i3)] + M5
 
     return Y
+end
+
+function s2z(S,Zo)
+    num_ports=size(S)[1]
+    nfreq=size(S)[3]
+    Z = zeros(Complex, num_ports, num_ports, nfreq)
+    Id = Matrix{Int64}(I, num_ports, num_ports)
+    for cont in range(1, stop=nfreq)
+        Z[:,:,cont]=Zo*((Id-1.0*S[:,:,cont])\(Id+S[:,:,cont]))
+    
+    end
+    return Z
+end
+
+function s2y(S,Zo)
+    num_ports=size(S)[1]
+    nfreq=size(S)[3]
+    Y = zeros(Complex, num_ports, num_ports, nfreq)
+    Id = Matrix{Int64}(I, num_ports, num_ports)
+    for cont in range(1, stop=nfreq)
+        Y[:,:,cont]=Zo*((Id+S[:,:,cont])\(Id-1.0*S[:,:,cont]))
+    end
+    return Y
+end
+
+function ComputeMatrixVector(w,escalings,A,Gamma,P_mat,Lp_x_mat,Lp_y_mat,Lp_z_mat,
+                        Z_self,Yle,invZ,invP,LU_S,x)
+
+    mx = size(Lp_x_mat)[1]
+    my = size(Lp_y_mat)[1]
+    mz = size(Lp_z_mat)[1]
+
+    m = mx + my + mz
+    ns = size(Gamma)[2]
+    n = size(Gamma)[1]
+    I = zeros(Complex, m, 1)
+    Q = zeros(Complex, ns, 1)
+    Phi = zeros(Complex, n, 1)
+    I[1:m, 1] = x[1:m]
+    Q[1:ns, 1] = x[m:m + ns]
+    Phi[1:n, 1] = x[m + ns:m + ns + n]
+    Y1 = zeros(Complex, m, 1)
+
+    ia1 = range(1, stop=mx)
+    ia2 = range(mx, stop=mx + my)
+    ia3 = range(mx + my, stop=mx + my + mz)
+
+    Y1[ia1] = 1j * w * escalings.Lp * dot(Lp_x_mat, I[ia1])
+    Y1[ia2] = 1j * w * escalings.Lp * dot(Lp_y_mat, I[ia2])
+    Y1[ia3] = 1j * w * escalings.Lp * dot(Lp_z_mat, I[ia3])
+
+    Y1=Y1+Z_self.*I+dot(sparse(A), Phi)
+
+    Y2 = escalings.P * np.dot(P_mat,Q) -1.0*dot(sparse(transpose(Gamma)), Phi)
+    Y3 = -1.0*(dot(sparse(transpose(A)), I)) + dot(sparse(Yle), Phi) +1j*w*(dot(sparse(Gamma), Q))
+
+    MatrixVector = precond_3_3_vector(LU_S, invZ, invP, A,Gamma, w, Y1, Y2, Y3)
+
+    return MatrixVector
+end
+
+function Quasi_static_iterative_solver(freq_in,A,Gamma,P_mat,Lp_x_mat,Lp_y_mat,Lp_z_mat,diag_R,diag_Cd,ports,lumped_elements,GMRES_settings)
+
+    escalings = escals(1e6, 1e-12, 1e-3, 1e12, 1e3, 1e3, 1e-9)
+    #escalings = escals(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0)
+
+    freq = freq_in * escalings.freq
+    # GMRES settings - ---------------------------
+    Inner_Iter = GMRES_settings.Inner_Iter
+    Outer_Iter = GMRES_settings.Outer_Iter
+    # -------------------------------------------
+
+    mx = size(Lp_x_mat)[1]
+    my = size(Lp_y_mat)[1]
+    mz = size(Lp_z_mat)[1]
+
+    m = mx + my + mz
+    n = size(A)[2]
+    ns = size(Gamma)[2]
+
+    w = 2 * mt.pi * freq
+
+    nfreq = size(w)[1]
+
+    Is = zeros(Float64, n, 1)
+
+    num_ports=size(ports.port_start)[1]
+
+    S = zeros(Complex, num_ports, num_ports, nfreq)
+
+    X_prec=zeros(Complex, m+n+ns, num_ports)
+
+    diag_P = zeros(Float64, ns)
+    for c in range(1, stop=ns)
+        diag_P[c]=escalings.P*P_mat[c,c]
+    end
+
+    diag_Lp = zeros(Float64, m)
+    for c in range(1, stop=mx)
+        diag_Lp[c] = escalings.Lp*Lp_x_mat[c, c]
+    end
+    for c in range(1, stop=my)
+        diag_Lp[c+mx] = escalings.Lp*Lp_y_mat[c, c]
+    end
+    for c in range(1, stop=mz)
+        diag_Lp[c+mx+my] = escalings.Lp*Lp_z_mat[c, c]
+    end
+
+    invP = sparse((range(1, stop=ns), range(1, stop=ns), 1. / diag_P))
+
+    val_chiusura = 50.0
+
+    diag_R=escalings.R*diag_R
+    diag_Cd=escalings.Cd * diag_Cd
+
+    for k in range(1, stop=nfreq)
+        print("Freq n=", k+1, " - Freq Tot=", nfreq)
+        Z_self = compute_Z_self(diag_R,diag_Cd, w[k])
+        Yle = build_Yle_S(lumped_elements, ports, escalings, n, w[k] / escalings.freq, val_chiusura)
+        invZ = sparse(range(1, stop=m), range(1, stop=m), (1. / (Z_self[:,0] + 1j * w[k] * diag_Lp)))
+
+        SS=Yle+dot(sparse(transpose(A)),dot(sparse(invZ),A))+1j*w[k]*dot(sparse(Gamma),dot(sparse(invP),transpose(Gamma)))
+
+        LU_S=lu(SS)
+        #LU_S = linalg.spilu(SS, drop_tol=1e-6, options=dict(SymmetricMode=True))
+
+        for c1 in range(1, stop=num_ports)
+            n1 = ports.port_nodes[c1, 1]
+            n2 = ports.port_nodes[c1, 2]
+            Is[n1] = 1.0 * escalings.Is
+            Is[n2] = -1.0 * escalings.Is
+
+            tn = precond_3_3_Kt(LU_S, invZ, invP, A,Gamma, m, ns, Is)
+
+            #counter = gmres_counter()
+
+            products_law = x -> ComputeMatrixVector( w[k], escalings, A,Gamma, P_mat,Lp_x_mat,Lp_y_mat,Lp_z_mat,Z_self, Yle, invZ,invP, LU_S,x)
+
+            prodts = LinearMap(products_law, n + m + ns, n + m + ns,)
+
+            V_o, info = gmres(prodts, tn, x=X_prec[:, c1], reltol=GMRES_settings.tol[k], restart=nothing, maxiter=Inner_Iter, log=true)
+
+            # V_o, info = linalg.gmres(prodts, tn, x0=X_prec[:, c1], tol=GMRES_settings.tol[k], \
+            #                               restart=None, maxiter=Inner_Iter, M=None, \
+            #                               callback=counter, restrt=None, atol=None, callback_type=None)
+
+            V = V_o
+            X_prec[:, c1]=V
+
+            Is[n1] = 0.0
+            Is[n2] = 0.0
+
+            if info == 0
+                print("convergence reached, number of iterations: ", counter.niter)
+            else
+                if info > 0
+                    print("convergence not reached, number of iterations: ", counter.niter)
+                else
+                    print("illegal input or breakdown, number of iterations: ", counter.niter)
+                end
+            end
+
+            for c2 in range(num_ports)
+                n3 = ports.port_nodes[c2, 1]
+                n4 = ports.port_nodes[c2, 2]
+
+                if c1 == c2
+                    S[c1, c2, k] = (2.0 * (V[m + ns + n3] - V[m + ns + n4]) - val_chiusura) / val_chiusura
+                else
+                    S[c1, c2, k] = (2.0 * (V[m + ns + n3] - V[m + ns + n4])) / val_chiusura
+                end
+
+                S[c2, c1, k] = S[c1, c2, k]
+            end
+        end
+    end
+    Z=s2z(S,val_chiusura)
+    Y=s2y(S,val_chiusura)
+    return Z,Y,S
 end
